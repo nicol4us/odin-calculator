3f7240f6d1cd3a51888df894ca3223226553450d
Nicolaus Dwi Noviantono <94541419+nicol4us@users.noreply.github.com>
2025-02-01 10:55:12 +0700

    Assign variable of operator, first & second number

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 
-// Constant declaration
+// Constant Declaration
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++
 const allNumbersButton = Array.from(document.querySelectorAll(".number"));
 const allOperatorButton = Array.from(document.querySelectorAll(".function"));
 const display = document.querySelector("#display");
+
+
+// Variable Declarations
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
+let firstNumber = {value : "", isActive : true};
+let operator = {value: "", isActive : false};
+let secondNumber = {value : "", isActive : false};
