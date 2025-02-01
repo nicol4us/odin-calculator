b75fe3df79bc816d8d7f4433bb62644da1baa036
Nicolaus Dwi Noviantono <94541419+nicol4us@users.noreply.github.com>
2025-02-01 18:24:17 +0700

    Add function setNumbersCalculatorListener

--- script.js
+++ script.js
@@ -6,8 +6,25 @@ const allOperatorButton = Array.from(document.querySelectorAll(".function"));
 const display = document.querySelector("#display");
 
 
+
+
 // Variable Declarations
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++
 let firstNumber = {value : "", isActive : true};
 let operator = {value: "", isActive : false};
 let secondNumber = {value : "", isActive : false};
+
+
+
+// Functions Declarations
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// (ArrayButtonElement, Object, Object, Object, divElement) -> ()
+// To active click button on Number and show it in display
+function setNumbersCalculatorListener(arrayNumber, firstNumber, secondNumber, operator, displayElement) {
+    arrayNumber.forEach(element => {
+        element.addEventListener("click", function(event) {
+            
+        })
+    });
+}
