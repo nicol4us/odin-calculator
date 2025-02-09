e2348413960278c43cf661ef95c2aac565d66b3c
Nicolaus Dwi Noviantono <94541419+nicol4us@users.noreply.github.com>
2025-02-09 19:29:40 +0700

    Add constant of operatorArray

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const clearButton = document.querySelector("#clear");
 const backspaceButton = document.querySelector("#backspace"); 
 const negateButton = document.querySelector("#negate");
 const commaButton = document.querySelector("#comma");
-const numbersArray = ["0","1","2","3","4","5","6","7","8","9"]
+const numbersArray = ["0","1","2","3","4","5","6","7","8","9"];
+const operatorArray = ["x", "/", "+","-", "="];
 
 
 // Variable Declarations
